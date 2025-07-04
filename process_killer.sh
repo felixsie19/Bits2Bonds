a8c11044085f57e1017ca57beb059f61935f515f
jonbind <89415196+jonbind@users.noreply.github.com>
2025-07-04 08:16:22 +0200

    Delete process_killer.sh

--- process_killer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-process_name="$1"  # Get the process name from the first command-line argument
-
-# Find processes with the given name and kill them
-pgrep -x "$process_name" | while read -r pid; do
-  kill -9 "$pid" && echo "Killed process $pid with name $process_name" || echo "Failed to kill process $pid with name $process_name"
-done 
