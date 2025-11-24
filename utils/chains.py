0dce12fbd78df4313d139b951aed17648bbcc047
jonbind <89415196+jonbind@users.noreply.github.com>
2025-11-24 11:19:57 +0100

    Update chains.py

--- utils/chains.py
+++ utils/chains.py
@@ -26,7 +26,7 @@ binary_name=sys.argv[3]
 with open('data/DFfromRL.pkl', 'rb') as f:
     data = pickle.load(f)
 
-df = pddataFrame(data)
+df = pd.dataFrame(data)
 
 # Initialize or set default values
 lipo_beads = None
