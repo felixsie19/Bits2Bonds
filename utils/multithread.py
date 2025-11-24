140affc74e753bf580da523596a2a44b4b2fea70
jonbind <89415196+jonbind@users.noreply.github.com>
2025-11-24 11:17:59 +0100

    Update multithread.py

--- utils/multithread.py
+++ utils/multithread.py
@@ -14,7 +14,7 @@ binary_name=sys.argv[1]
 iteration_number = int(os.environ.get('ITERATION_NUMBER'))
 def process_iteration(iteration_num, run):
     try:
-        args = ["python3", "./utils/chains.py", str(iteration_num), str(run)]
+        args = ["python3", "./utils/chains.py", str(iteration_num), str(run),str(binary_name)]
         print(f"Starting process for iteration {iteration_num}, run {run}: {args}")
         # Create the subprocess with a timeout
         process = subprocess.Popen(args)
