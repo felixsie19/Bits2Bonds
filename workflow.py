9191f76581744fe9af4c40436722301f78d716ec
jonbind <89415196+jonbind@users.noreply.github.com>
2025-11-24 08:33:09 +0100

    Add files via upload

--- workflow.py
+++ workflow.py
@@ -1,19 +1,14 @@
 import subprocess
 import os.path
-from utils import new_GA
-from utils import rl
-from utils import make_pdf
+from ga import new_GA
+import rl
 import pandas as pd
-from utils import exchange_beads
+from bead_exchanger import exchange_beads
 import sys
 
 
-##################Set Name of Gromacs Binary##########################
-
-binary_name=sys.argv[1]
-
 # Specify the output file
-output_file = "./data/output.txt"
+output_file = "output.txt"
 
 # Open the file in write mode 'w'
 with open(output_file, 'w') as f:
@@ -39,15 +34,15 @@ with open(output_file, 'w') as f:
     print("Moldesigner ready")
     df=exchange_beads(8,df,0.5)
     df=exchange_beads(4,df,0.5)
-    df.to_pickle("./data/DFfromRL.pkl")
+    df.to_pickle("DFfromRL.pkl")
 
 ## 2. Check for the presence of 'model1.pkl'
     for i in range(0,50):
 
-        if os.path.exists("./data/DFfromRL.pkl"):
+        if os.path.exists("DFfromRL.pkl"):
             try:
                 os.environ['ITERATION_NUMBER'] = str(i)
-                subprocess.run(["python3", "./utils/multithread.py", binary_name], check=True)
+                subprocess.run(["python3", "multithread3.py"] ,check=True)  # Will raise exception on error
                 print(f"Finished iteration {i+1}")
             except subprocess.CalledProcessError as e:
                 print(f"Error in multithread.py: {e}")
@@ -59,21 +54,18 @@ with open(output_file, 'w') as f:
 #
 #
         ## Load the dataframe and remove duplicates initially
-        df = pd.read_pickle("./data/DFfromRL.pkl")
+        df = pd.read_pickle("DFfromRL.pkl")
     
         # Check if the 'performance_score' column exists
         if 'performance_score' not in df.columns:
-            print("Error: 'performance_score' column not found in the dataFrame.")
+            print("Error: 'performance_score' column not found in the DataFrame.")
             sys.exit(1)  # Exit with an error code (non-zero)
     
         # Check if *any* value in the 'performance_score' column is greater than 9
-        if (df['performance_score'] > 17).any():
+        if (df['performance_score'] > 24).any():
             print("Success: At least one performance score is greater than 9.")
             df[['performance_score', 'Performance_siRNA_pH_4', 'Performance_double_membrane', 'Performance_siRNA_pH_8',
-               'beads_hydro', 'beads_lipo']].to_csv(f"./data/output_df_episode_{i}.csv")
-            
-            #make PDF
-            make_pdf.make_pdf_file(i)
+               'beads_hydro', 'beads_lipo']].to_csv(f"output_df_episode_{i}.csv")
             sys.exit(0)  # Exit with a success code (0)
         else:
             print("Continuing: No performance score is greater than 9.")
@@ -97,7 +89,7 @@ with open(output_file, 'w') as f:
             print(ga.parents["performance_score"])
             output_df=ga.input_df
             output_df[['performance_score', 'Performance_siRNA_pH_4', 'Performance_double_membrane', 'Performance_siRNA_pH_8',
-               'beads_hydro', 'beads_lipo']].to_csv(f"./data/output_df_episode_{i}.csv")
+               'beads_hydro', 'beads_lipo']].to_csv(f"output_df_episode_{i}.csv")
             # Export lead and mutate
             ga.export_lead(i)
             new_pol = ga.mutate(2, 1)
@@ -107,7 +99,7 @@ with open(output_file, 'w') as f:
             df = exchange_beads(4, df, 0.5)
             # Remove duplicates before saving
             
-            df.to_pickle("./data/DFfromRL.pkl")
+            df.to_pickle("DFfromRL.pkl")
             print(f"Starting Challenges in iteration {i+1}")
             print(f"Starting iteration {i+1}",flush=True)
 
