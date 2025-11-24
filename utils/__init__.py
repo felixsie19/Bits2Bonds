71f6b1e2308cc21a9ce1da3f042f91094b6d1826
jonbind <89415196+jonbind@users.noreply.github.com>
2025-11-24 10:46:25 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .bead_exchanger import exchange_beads 
 from .ga import new_GA
-from .sa_score_calculator import apply_sa_score_penalty
+from utils.sa_score_calculator import apply_sa_score_penalty
